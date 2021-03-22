9c195a2331a870f493b1d8d3967fad95a3a7bb72
andrewkramer5 <32948446+andrewkramer5@users.noreply.github.com>
2021-03-22 14:41:45 -0400

    Created intellij project

---
+++ src/main/java/edu/purdue/cs/groupwallet/GroupwalletApplication.java
@@ -0,0 +1,13 @@
+package edu.purdue.cs.groupwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupwalletApplication.class, args);
+	}
+
+}
